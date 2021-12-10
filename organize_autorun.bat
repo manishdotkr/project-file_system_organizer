82f9476f4996b88217ac0763923d113a2975a0af
Manish <officialmanishkr98@gmail.com>
2021-12-10 20:35:34 +0530

    first commit

---
+++ organize_autorun.bat
@@ -0,0 +1,1 @@
+node fso.js organize "C:\Users\Manish\Desktop\codes\Projects\FIle System Organizer\test_folder"
---
+++ tree_autorun.bat
@@ -0,0 +1,3 @@
+node fso.js tree "C:\Users\Manish\Desktop\codes\Projects\FIle System Organizer\test_folder"
+
+node
